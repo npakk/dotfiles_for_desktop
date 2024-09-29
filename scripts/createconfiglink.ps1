f46ed25712b9b6c79d3384b047375bfb8a5a18f7
npakk <npakk669@gmail.com>
2024-09-29 22:39:38 +0900

    change

--- scripts/createconfiglink.ps1
+++ scripts/createconfiglink.ps1
@@ -1,6 +1,6 @@
 $configPath = [Environment]::GetFolderPath("User")
 $wsShell = New-Object -ComObject WScript.Shell
-$shortcutPath = $configPath + "\scoop" + "\config.json.lnk"
+$shortcutPath = $configPath + "\.config\scoop\config.json.lnk"
 $shortcut = $wsShell.CreateShortcut($shortcutPath)
 $shortcut.TargetPath = (Convert-Path .) + "\.config\scoop\config.json"
 $shortcut.Save()
